e2645fcc3c9a30fb774a9d1241d8a99a3bb9d84a
irconde <ivanrodriguezconde@gmail.com>
2019-10-03 18:13:43 -0500

    getting result data

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -1,12 +1,17 @@
 package com.ualr.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int QR_CODE_REQUEST = 1;
+    static final String QR_CODE_KEY = "QRCode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // TODO 01. We must launch new activity using startActivityForResult
     public void onButtonClick(View view) {
         Intent intent = new Intent(this, TargetActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, QR_CODE_REQUEST);
+    }
+
+    // TODO 05. Get data sent from the second activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == QR_CODE_REQUEST && resultCode == RESULT_OK) {
+            String qrCode = data.getStringExtra(QR_CODE_KEY);
+            TextView codeText = findViewById(R.id.codeText);
+            codeText.setText(qrCode);
+        }
     }
 }
--- app/src/main/java/com/ualr/activities/TargetActivity.java
+++ app/src/main/java/com/ualr/activities/TargetActivity.java
@@ -1,7 +1,9 @@
 package com.ualr.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -9,9 +11,22 @@ import androidx.annotation.Nullable;
  * Created by irconde on 2019-10-02.
  */
 public class TargetActivity extends Activity {
+
+    private final static String QR_CODE = "59012341234567";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
+    }
+
+    public void onButtonClick(View view) {
+        // TODO 02. We put in the intent's bundle the data we want to return to the
+        //  first activity
+        Intent intent = getIntent();
+        intent.putExtra(MainActivity.QR_CODE_KEY, QR_CODE);
+        // TODO 03. We invoke setResult method to send data back to the first activity
+        setResult(RESULT_OK, intent);
+        // TODO 04. Close the activity
+        finish();
     }
 }
