919d6b573c502d97e455ec6e91145d147d65d69f
irconde <ivanrodriguezconde@gmail.com>
2019-10-04 10:13:03 -0500

    Music player launch

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.ualr.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,11 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
-        String startPoint = "42.322591, -7.865459";
-        String endPoint= "42.344961, -7.856006";
-        String routeUri = String.format("http://maps.google.com/maps?saddr=%s&daddr=%s", startPoint, endPoint);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(routeUri));
-        startActivity(intent);
+        Intent playerIntent = new Intent("android.intent.action.MUSIC_PLAYER");
+        startActivity(playerIntent);
     }
 
 }
