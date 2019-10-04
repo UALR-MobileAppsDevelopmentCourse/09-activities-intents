8f8c9fa191d8666eeeb72dff9d7fb565af4f5c4a
irconde <ivanrodriguezconde@gmail.com>
2019-10-04 09:06:29 -0500

    Geomapping coordinates

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -16,9 +16,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
-        String place = "University of Arkansas at Little Rock";
-        String placeUri = String.format("geo:0,0?q=(%s)", place);
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(placeUri));
+        String geoCode = "geo:42.336408, -7.863180?z=16";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoCode));
         startActivity(intent);
     }
 
