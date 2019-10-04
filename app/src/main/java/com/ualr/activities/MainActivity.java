cfa78c36c6c3bc0e97efcdc0b4a045497ef4a86d
irconde <ivanrodriguezconde@gmail.com>
2019-10-04 16:26:49 -0500

    Contact select and query

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -1,13 +1,18 @@
 package com.ualr.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CONTACT_REQUEST_CODE = 111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,12 +20,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View view) {
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-        sendIntent.setType("text/plain");
-        Intent shareIntent = Intent.createChooser(sendIntent, null);
-        startActivity(shareIntent);
+        String contactsData = "content://contacts/people/";
+        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse(contactsData));
+        // TODO 01. Start the contacts app to select a contact
+        startActivityForResult(intent, CONTACT_REQUEST_CODE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CONTACT_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                String returnedData = data.getDataString();
+                // TODO 02. Once a contact has been selected, we get the returned data an put it in the bundle
+                // of other intent to get the information of the corresponding contact
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(returnedData));
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "No contact was selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
