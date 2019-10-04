4cae593fde562c4a121897198c7bedc914c3938b
irconde <ivanrodriguezconde@gmail.com>
2019-10-03 21:35:01 -0500

    Open phone dialer from activity

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -1,17 +1,13 @@
 package com.ualr.activities;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    static final int QR_CODE_REQUEST = 1;
-    static final String QR_CODE_KEY = "QRCode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // TODO 01. We must launch new activity using startActivityForResult
     public void onButtonClick(View view) {
-        Intent intent = new Intent(this, TargetActivity.class);
-        startActivityForResult(intent, QR_CODE_REQUEST);
+        String phoneNumberUri = "tel:501-432-4129";
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumberUri));
+        startActivity(intent);
     }
-    
-    // TODO 05. Get data sent from the second activity
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == QR_CODE_REQUEST && resultCode == RESULT_OK) {
-            String qrCode = data.getStringExtra(QR_CODE_KEY);
-            TextView codeText = findViewById(R.id.codeText);
-            codeText.setText(qrCode);
-        }
-    }
+
 }
