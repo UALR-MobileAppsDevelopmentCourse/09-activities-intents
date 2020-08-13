ed83de25bf37d06d7c4d4e1d6c2e2e2bc7f3a934
irconde <irconde@ualr.edu>
2020-08-13 15:24:08 -0500

    Libraries' version updated

--- app/src/main/java/com/ualr/activities/MainActivity.java
+++ app/src/main/java/com/ualr/activities/MainActivity.java
@@ -1,12 +1,9 @@
 package com.ualr.activities;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
